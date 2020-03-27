64e5086d4a7b5393b911f73bc17c4804e55d33a2
Néstor Andrés Peña <nestor2509@gmail.com>
2020-03-27 14:44:25 -0500

    pagina uno terminada

--- src/components/paginas/Pagina01.js
+++ src/components/paginas/Pagina01.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import {TimelineMax} from 'gsap';
 
+import titulo from '../../img/ilustraciones/titulo.png';
+import buscamos from '../../img/ilustraciones/buscamos.png';
+import logo from '../../img/ilustraciones/logo.png';
+import hash from '../../img/ilustraciones/hash.png';
+
 class Pagina01 extends Component {
     constructor(props) {
         super(props);
@@ -9,25 +14,47 @@ class Pagina01 extends Component {
 
         this.titulo_elem = null;
         this.subtitulo_elem = null;
-        this.logo_elem = null;
-        this.hash_elem = null;
+        this.btm = null;
     }
 
     componentDidMount() {
         this.tweenIntro = new TimelineMax();
         this.tweenIntro.add('start')
-        .fromTo(this.titulo_elem, 0.5, {autoAlpha: 0}, {autoAlpha: 1})
+        .fromTo(this.titulo_elem, 1, {autoAlpha: 0}, {autoAlpha: 1}, '+=1')
+        .fromTo(this.subtitulo_elem, 0.25, {autoAlpha: 0}, {autoAlpha: 1})
+        .fromTo(this.btm, 0.25, {autoAlpha: 0}, {autoAlpha: 1})
         ;
     }
 
     render() {
         return(
             <>
-                <div className="slide">
-                    <div className="titulo" ref={div => this.titulo_elem = div}></div>
-                    <div className="subtitulo" ref={div => this.subtitulo_elem = div}></div>
-                    <div className="logo" ref={div => this.logo_elem = div}></div>
-                    <div className="hash" ref={div => this.hash_elem = div}></div>
+                <div className="slide pagina_01">
+                    <div
+                        className="titulo imgcontainer"
+                        ref={div => this.titulo_elem = div}
+                        style={{backgroundImage: `url(${titulo})`}}    
+                    ></div>
+                    <div
+                        className="subtitulo imgcontainer"
+                        ref={div => this.subtitulo_elem = div}
+                        style={{backgroundImage: `url(${buscamos})`}}
+                    ></div>
+
+                    <div
+                        className="btm"
+                        ref={div => this.btm = div}
+                    >
+                        <div
+                            className="logo imgcontainer"
+                            style={{backgroundImage: `url(${logo})`}}
+                        ></div>
+                        <div
+                            className="hash imgcontainer"
+                            style={{backgroundImage: `url(${hash})`}}
+                        ></div>
+                    </div>
+                    
                 </div>
             </>
         );
